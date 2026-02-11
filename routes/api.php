57c421c5fcfb06e2e4047d2e6bdc2c89a7c219ef
nibal <nebal.ps@gmail.com>
2026-02-11 10:24:53 +0200

    first commit

--- routes/api.php
+++ routes/api.php
@@ -11,3 +11,9 @@ Route::post('/login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/update-db', function() {
+    // استخدمنا fresh بدلاً من refresh
+    // fresh: يحذف الجداول فوراً دون النظر لدالة down
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh --seed --force');
+    return 'Database Wiped & Re-Created Successfully with Full Details!';
+});
