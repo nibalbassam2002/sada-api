cd2da976308ac56a50beff23c2371a86272be884
nibalbassam2002 <nebal.ps@gmail.com>
2026-05-02 10:51:57 +0300

    first commit

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['https://sada-frontend-vvv7.vercel.app', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
